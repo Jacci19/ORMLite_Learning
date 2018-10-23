152c0bca508d2a7c422322078bce5cf8c0e6523b
Jacci19 <jacek205@wp.pl>
2018-10-23 11:48:36 +0200

    ORMLite learning - Lesson 3 (tableUtils)

---
+++ Backup/example - lesson 2/Main.java
@@ -0,0 +1,41 @@
+//https://www.youtube.com/watch?v=0xCpwNbbBc8&list=PLpzwMkmxJDUzSwjdC5nVEY9h3rdfgXX7V
+//SQLite Studio - tego używam
+
+package pl.ormlite.example;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, IOException {
+
+        String databaseUrl = "jdbc:sqlite:database.db";                                     //sterownik do bazy danych (BD) : nazwa BD (ew. poprzedzona ścieżką), tu będzie w głównym katalogu projektu
+        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);          //połączenie z naszą bazą danych
+
+        /*
+        String databaseUrlH2 = "jdbc:h2:./database";
+        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlH2, "userName", "password");
+         -  tak to wygląda dla H2. W pom trzeba jeszcze wkleić odpowiednie dependency (z mvnrepository.com). Pola: "userName", "password" są opcjonalne.
+        z www.h2database.com trzeba ściągnąć klienta żeby obsłużyć taką bazę
+        */
+
+        Dao<Account, String> accountDao = DaoManager.createDao(connectionSource, Account.class);
+        TableUtils.createTable(connectionSource, Account.class);
+
+        Account account = new Account();
+        account.setName("Jim Coakley");
+        account.setPassword("tajne");
+        accountDao.create(account);
+
+
+        connectionSource.close();
+    }
+
+}
--- src/main/java/pl/ormlite/example/Main.java
+++ src/main/java/pl/ormlite/example/Main.java
@@ -1,38 +1,35 @@
 //https://www.youtube.com/watch?v=0xCpwNbbBc8&list=PLpzwMkmxJDUzSwjdC5nVEY9h3rdfgXX7V
+//SQLite Studio - tego używam
 
 package pl.ormlite.example;
 
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.TableUtils;
+        import com.j256.ormlite.dao.Dao;
+        import com.j256.ormlite.dao.DaoManager;
+        import com.j256.ormlite.jdbc.JdbcConnectionSource;
+        import com.j256.ormlite.support.ConnectionSource;
+        import com.j256.ormlite.table.TableUtils;
 
-import java.io.IOException;
-import java.sql.SQLException;
+        import java.io.IOException;
+        import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException, IOException {
-        String databaseUrl = "jdbc:sqlite:database.db";                         //sterownik do bazy danych (BD) : nazwa BD (ew. poprzedzona ścieżką), tu będzie w głównym katalogu projektu
-        //String databaseUrlH2 = "jdbc:h2:./database";                          //tak to wygląda w przypadku bazy H2
 
-        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
-        /*ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrlH2, "userName", "password");
-        tak to wygląda dla H2. W pom trzeba jeszcze wkleić odpowiednie dependency (z mvnrepository.com). "userName", "password" są opcjonalne.
-        z www.h2database.com trzeba ściągnąć klienta żeby obsłużyć taką bazę
-        */
+        String databaseUrl = "jdbc:sqlite:database.db";                                     //sterownik do bazy danych (BD) : nazwa BD (ew. poprzedzona ścieżką), tu będzie w głównym katalogu projektu
+        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);          //połączenie z naszą bazą danych
 
-        Dao<Account, String> accountDao = DaoManager.createDao(connectionSource, Account.class);
-        TableUtils.createTable(connectionSource, Account.class);
-
-        Account account = new Account();
-        account.setName("Jim Coakley");
-        account.setPassword("tajne");
-        accountDao.create(account);
-
+        TableUtils.dropTable(connectionSource, Book.class, true);                //przebudowa BD, usuwamy starą tabelę, żeby potem stworzyć nową.
+        TableUtils.createTable(connectionSource, Book.class);                               //podpięcie tabeli do BD poprzez ORMLite
 
         connectionSource.close();
     }
 
 }
+
+/*
+TableUtils.createTableIfNotExists(connectionSource, Book.class);                    //tworzy tabelę tylko wtedy gdy ona nie istnieje (chroni przed nadpisaniem starej)
+
+jeśli próbujemy stworzyć tabelę w BD a ona już istnieje w BD to wyskoczy wyjątek.
+
+*/
